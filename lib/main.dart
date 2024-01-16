a3a22b956d4bf4350e41229957f4d2a1016daff6
satyasolves <satyasolvesolutions@gmail.com>
2024-01-15 23:32:55 -0600

    optimized main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyGoogleLikeApp());
+  runApp(GoogleAppClone());
 }
 
-class MyGoogleLikeApp extends StatelessWidget {
+class GoogleAppClone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Google-like App',
+      title: 'Google App Clone',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: GoogleHomePage(),
     );
@@ -26,63 +27,56 @@ class _GoogleHomePageState extends State<GoogleHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        title: Text('Google-like App'),
-    actions: <Widget>[
-    IconButton(
-    icon: Icon(Icons.search),
-    onPressed: () {
-    // Implement search functionality
-    },
-    ),
-    IconButton(
-    icon: Icon(Icons.more_vert),
-    onPressed: () {
-    // Implement more options
-    },
-    ),
-    ],
-    ),
-    drawer: Drawer(
-    child: ListView(
-    padding: EdgeInsets.zero,
-    children
-        : <Widget>[
-      DrawerHeader(
-        decoration: BoxDecoration(
-          color: Colors.blue,
-        ),
-        child: Text(
-          'Drawer Header',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
+      appBar: AppBar(
+        title: Text('Google App Clone'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // TODO: Implement search functionality
+            },
           ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+              // TODO: Implement more options
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Navigation',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
         ),
       ),
-      ListTile(
-        leading: Icon(Icons.message),
-        title: Text('Messages'),
-      ),
-      ListTile(
-        leading: Icon(Icons.account_circle),
-        title: Text('Profile'),
-      ),
-      ListTile(
-        leading: Icon(Icons.settings),
-        title: Text('Settings'),
-      ),
-    ],
-    ),
-    ),
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Item ${index + 1}'),
-            subtitle: Text('Subtitle of Item ${index + 1}'),
-          );
-        },
+      body: Center(
+        child: Text('Welcome to Google App Clone'),
       ),
     );
   }
