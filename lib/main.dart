419f7e8ab175673aaba9ccc5289b370596fe19e2
satyasolves <satyasolvesolutions@gmail.com>
2024-01-26 23:43:57 -0600

    added dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,27 @@ class MyGoogleCloneApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Google Clone',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      theme: ThemeData.light(), // Default light theme
+      darkTheme: ThemeData.dark().copyWith( // Custom dark theme
+        // Define dark theme colors here
+        primaryColor: Colors.grey[900],
+        scaffoldBackgroundColor: Colors.grey[900],
+        hintColor: Colors.grey[500],
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: Colors.grey[800],
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            primary: Colors.white,
+          ),
+        ),
       ),
+      themeMode: ThemeMode.dark, // Use dark theme
       home: GoogleHomePage(),
     );
   }
@@ -23,7 +40,6 @@ class GoogleHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Google's background color is white
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -46,8 +62,8 @@ class GoogleLogo extends StatelessWidget {
     return Text(
       'Google',
       style: TextStyle(
-        color: Colors.black,
-        fontSize: 90, // Google's logo is quite large
+        color: Colors.white,
+        fontSize: 90,
         fontWeight: FontWeight.w500,
       ),
     );
@@ -60,19 +76,17 @@ class SearchBar extends StatelessWidget {
     return Container(
       width: MediaQuery.of(context).size.width * 0.5, // Responsive width
       child: TextField(
-        style: TextStyle(fontSize: 16, color: Colors.black),
+        style: TextStyle(fontSize: 16, color: Colors.white),
         decoration: InputDecoration(
           hintText: 'Search Google or type a URL',
-          hintStyle: TextStyle(color: Colors.grey[800]),
-          filled: true,
-          fillColor: Colors.white,
+          hintStyle: TextStyle(color: Theme.of(context).hintColor),
           contentPadding: EdgeInsets.symmetric(vertical: 15),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(30),
             borderSide: BorderSide.none,
           ),
-          prefixIcon: Icon(Icons.search, color: Colors.grey[600]),
-          suffixIcon: Icon(Icons.mic, color: Colors.grey[600]),
+          prefixIcon: Icon(Icons.search, color: Colors.grey[500]),
+          suffixIcon: Icon(Icons.mic, color: Colors.grey[500]),
         ),
       ),
     );
@@ -88,13 +102,11 @@ class SearchButtons extends StatelessWidget {
         TextButton(
           onPressed: () {},
           child: Text('Google Search'),
-          style: TextButton.styleFrom(primary: Colors.grey[850]),
         ),
         SizedBox(width: 10),
         TextButton(
           onPressed: () {},
           child: Text("I'm Feeling Lucky"),
-          style: TextButton.styleFrom(primary: Colors.grey[850]),
         ),
       ],
     );
